b21186bff6791152869c3750a11fa169f7160255
Zachary Ogulnick <zacharyogulnick@gmail.com>
2024-10-04 15:50:51 -0500

    Created Spring Boot project.

---
+++ campus_rating_system/src/test/java/com/campus_rating_system/CampusRatingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus_rating_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRatingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
