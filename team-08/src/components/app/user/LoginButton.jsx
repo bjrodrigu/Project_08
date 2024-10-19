787e422c040f070e1e3b2bc18afe2366c7ef019d
Pranav <pnair22@wisc.edu>
2024-10-18 20:24:50 -0500

    more rebasing

--- team-08/src/components/app/user/LoginButton.jsx
+++ team-08/src/components/app/user/LoginButton.jsx
@@ -6,9 +6,8 @@ import { useNavigate } from 'react-router-dom';
 export default function LoginButton() {
       const navigate = useNavigate();
 
-// Login Button Component
-export default function LoginButton() {
-      return (<Button variant="outline-secondary"
+      return(
+            <Button variant="outline-secondary"
                   style={{left: '91vw', 
                   position: 'fixed', 
                   width: '4vw', 
