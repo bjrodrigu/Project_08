e450e1dd2dacf16a0c70d75fbb649819285d4128
Rithik Rajaram <rrajaram2@wisc.edu>
2024-10-18 22:04:44 -0500

    Added all Hibernate stuff

---
+++ backend/src/main/java/com/campus_rating_system/entities/Task.java
@@ -0,0 +1,57 @@
+package com.campus_rating_system.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Integer taskId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LocationTask> locationTasks;
+
+    // Getters and setters
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<LocationTask> getLocationTasks() {
+        return locationTasks;
+    }
+
+    public void setLocationTasks(List<LocationTask> locationTasks) {
+        this.locationTasks = locationTasks;
+    }
+}
