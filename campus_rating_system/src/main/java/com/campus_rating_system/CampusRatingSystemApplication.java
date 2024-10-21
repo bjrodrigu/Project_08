398b0559d26a7e7cf5620fe96c5e81873df76827
Ethan Yang <elyang3@wisc.edu>
2024-10-21 17:05:07 -0500

    fixed indentation again

--- campus_rating_system/src/main/java/com/campus_rating_system/CampusRatingSystemApplication.java
+++ campus_rating_system/src/main/java/com/campus_rating_system/CampusRatingSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CampusRatingSystemApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CampusRatingSystemApplication.class, args);
     }
 
