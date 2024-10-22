830b91d8b6571119ed531242124a4fe04cf74adf
Jinyu Shi <jshi278@wisc.edu>
2024-10-21 21:56:47 -0500

    update user page, design a the back button, changePassword button, finished pagination, and connect them to make whole page functioning.

--- team-08/src/components/app/user/UserOverlay.jsx
+++ team-08/src/components/app/user/UserOverlay.jsx
@@ -1,15 +1,18 @@
 import { DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
 import { useLoginState } from "../../contexts/LoginContext";
-import { useLocation } from "react-router";
+import { useLocation} from "react-router";
+import { useNavigate } from 'react-router-dom';
 import LoginButton from "./LoginButton";
 import RegisterButton from "./RegisterButton";
 import useLogout from "../../auth/useLogout";
+import BadgerUser from "../BadgerUser";
+
 //
 // Component displays user profile dropdown on hover
 export default function ReactRegisterButton() {
       // consume current outlet
       let location = useLocation();
-
+      const navigate = useNavigate();
       // const {login, setLogin, user, setUser} = useLoginState();
       const { user, setUser, login, setLogin } = useLoginState();
       const handleLogout = useLogout();
@@ -32,6 +35,11 @@ export default function ReactRegisterButton() {
                               User
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
+                              <Dropdown.Item
+                                    variant="outline-secondary" 
+                                    onClick={() => {navigate("/userProfile")}}>
+                                    Your Profile
+                              </Dropdown.Item>
                               <Dropdown.Item variant="outline-secondary" >
                                     Favorites
                               </Dropdown.Item>
@@ -47,6 +55,9 @@ export default function ReactRegisterButton() {
       // show register on login page
       else if (location.pathname == 'login') {
             return <RegisterButton />;
+      }
+      else if (location.pathname == '/userProfile') {
+            return <></>
       }
       // default case, return login
       else {
