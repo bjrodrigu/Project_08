33013bfb701267adb5e007f43442f116159ddea7
Ethan Yang <elyang3@wisc.edu>
2024-11-12 12:06:04 -0600

    matched spacing

--- backend/src/main/java/com/campus_rating_system/CampusRatingSystemApplication.java
+++ backend/src/main/java/com/campus_rating_system/CampusRatingSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CampusRatingSystemApplication {
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CampusRatingSystemApplication.class, args);
 	}
